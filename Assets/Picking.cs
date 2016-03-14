3e4d0e353847f59be9b39a410a8973621b8ecba0
Kevin Tessier <tess0112@algonquinlive.com>
2016-03-14 12:17:50 -0400

    Lab6 Complete
    Can eliminate the teeth.

---
+++ Assets/Picking.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Picking : MonoBehaviour {
+
+    public Camera pickingCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0) == true)
+        {
+            // Create a ray at the mouse position.
+            Vector3 mousePosition = Input.mousePosition;
+            Ray pickingRay = pickingCamera.ScreenPointToRay(mousePosition);
+
+            // Use the ray to see if anything is hit.
+            RaycastHit hit;
+            bool success = Physics.Raycast(pickingRay, out hit);
+
+            if (success)
+            {
+                Debug.Log("The name of the picked object is " + hit.collider.gameObject.name);
+                if (hit.collider.gameObject.tag == "Tooth")
+                {
+                    hit.collider.gameObject.SetActive(false);
+                }
+            }
+        }
+	}
+}
---
+++ Assets/Tilt.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tilt : MonoBehaviour {
+  private Vector2 _initialMousePosition;
+  public float rotationConstraint = 36.0f;
+
+	// Use this for initialization
+    void Start () {
+        _initialMousePosition = Input.mousePosition;
+	}
+
+  // Update is called once per frame
+  void Update() {
+    Vector2 currentMousePosition = Input.mousePosition;
+    Vector2 delta = currentMousePosition - _initialMousePosition;
+
+    if(Mathf.Abs(delta.x) > rotationConstraint) {
+      delta.x = Mathf.Sign(delta.x) * rotationConstraint;
+    }
+
+    if(Mathf.Abs(delta.y) > rotationConstraint) {
+      delta.y = Mathf.Sign(delta.y) * rotationConstraint;
+    }
+
+    Vector3 localRotation = new Vector3(delta.y, 0.0f, -delta.x);
+    transform.localRotation = Quaternion.Euler(localRotation);
+	}
+}
